c3ce25d88cad97a6a8d403ac570d84562070fd00
Vishal Bhardwaj <vishalbhardwajformal@gmail.com>
2025-09-22 12:04:31 +0530

    Initial build process

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Simple React Webpage</h1>
       </header>
+
+      <main>
+        <p>This is a simple webpage created using React.</p>
+        <button onClick={() => alert("Button clicked!")}>Click Me</button>
+      </main>
+      
+      <footer className="App-footer">
+        <p>&copy; 2025 My Simple React Webpage</p>
+      </footer>
     </div>
   );
 }
